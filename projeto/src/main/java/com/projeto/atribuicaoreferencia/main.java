b0d11cfc12d1534cf1dd9a20cc9d83b0d1173888
Marcelo Bentes <marcelo.maia.m@gmail.com>
2023-02-27 22:26:07 -0300

    adicionando referencia

---
+++ projeto/src/main/java/com/projeto/atribuicaoreferencia/main.java
@@ -0,0 +1,13 @@
+package com.projeto.atribuicaoreferencia;
+
+public class main {
+
+    public static void main(String[] args) {
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println("intA= "+intA+ " intB= "+intB);
+        intA =2;
+        System.out.println("intA= "+intA+ " intB= "+intB);
+    }
+}
